3ed71a5cd6aae1ade56dc98f1b480b7e14d185e0
MohamedCK <abdulhakimm280@gmail.com>
2022-09-10 09:21:18 -0400

    Render second section on javascript

---
+++ JS/main.js
@@ -0,0 +1,87 @@
+const cardSection = document.querySelector('.part-3');
+
+cardSection.innerHTML += `
+<div class="container1">
+<h2 class="second-header">My Recent Works</h2>
+<div class="cards">
+    <div class="card">
+        <img src="./Images/cardimg.jpg" alt="">
+        <div class="txtcontainer">
+            <h3>Multi-Post Stories <br> Gain+Glory</h3>
+            <ul class="item-lists">
+                <li>Ruby on rails</li>
+                <li>css</li>
+                <li>JavaScript</li>
+                <li>html</li>
+            </ul>
+            <button>See Project</button>
+        </div>
+    </div>
+    <div class="card">
+        <img src="./Images/cardimg.jpg" alt="">
+        
+        <div class="txtcontainer">
+            <h3>Multi-Post Stories <br> Gain+Glory</h3>
+            <ul class="item-lists">
+                <li>Ruby on rails</li>
+                <li>css</li>
+                <li>JavaScript</li>
+                <li>html</li>
+            </ul>
+            <button>See Project</button>
+        </div>
+    </div>
+    <div class="card">
+        <img src="./Images/cardimg.jpg" alt="">
+        <div class="txtcontainer">
+            <h3>Multi-Post Stories <br> Gain+Glory</h3>
+            <ul class="item-lists">
+                <li>Ruby on rails</li>
+                <li>css</li>
+                <li>JavaScript</li>
+                <li>html</li>
+            </ul>
+            <button>See Project</button>
+        </div>
+    </div>
+    <div class="card">
+        <img src="./Images/cardimg.jpg" alt="">
+        <div class="txtcontainer">
+            <h3>Multi-Post Stories <br> Gain+Glory</h3>
+            <ul class="item-lists">
+                <li>Ruby on rails</li>
+                <li>css</li>
+                <li>JavaScript</li>
+                <li>html</li>
+            </ul>
+            <button>See Project</button>
+        </div>
+    </div>
+    <div class="card">
+        <img src="./Images/cardimg.jpg" alt="">
+        <div class="txtcontainer">
+            <h3>Multi-Post Stories <br> Gain+Glory</h3>
+            <ul class="item-lists">
+                <li>Ruby on rails</li>
+                <li>css</li>
+                <li>JavaScript</li>
+                <li>html</li>
+            </ul>
+            <button>See Project</button>
+        </div>
+    </div>
+    <div class="card">
+        <img src="./Images/cardimg.jpg" alt="">
+        <div class="txtcontainer">
+            <h3>Multi-Post Stories <br> Gain+Glory</h3>
+            <ul class="item-lists">
+                <li>Ruby on rails</li>
+                <li>css</li>
+                <li>JavaScript</li>
+                <li>html</li>
+            </ul>
+            <button>See Project</button>
+        </div>
+</div>
+</div>
+`
